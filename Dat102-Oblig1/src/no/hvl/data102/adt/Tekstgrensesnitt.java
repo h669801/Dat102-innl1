f7898aa2580c99a369f0be3d81474f27996540f1
thoma <thoma@158.37.198.152>
2023-01-26 12:58:39 +0100

    ..

---
+++ Dat102-Oblig1/src/no/hvl/data102/adt/Tekstgrensesnitt.java
@@ -0,0 +1,10 @@
+package no.hvl.data102.adt;
+
+public class Tekstgrensesnitt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Dat102-Oblig1/src/no/hvl/data102/klient/FilmarkivADT.java
@@ -0,0 +1,10 @@
+package no.hvl.data102.klient;
+
+public class FilmarkivADT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
